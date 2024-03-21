a538316914c4fb4d9d1f1aee719079b46f9efbf9
Alessandro Liberatoscioli <alessandro.liberatoscioli.g@gmail.com>
2024-03-21 22:35:35 +0100

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar'; // Importa tu componente de barra de navegación aquí
-import Home from './Home'; // Importa tu componente de página de inicio u otra página principal aquí
+// import Home from './Home'; // Importa tu componente de página de inicio u otra página principal aquí
 import TechnicianRegistration from './TechnicianRegistration'; // Importa tu componente de registro de técnicos aquí
 import AdminTasksManagement from './AdminTasksManagement'; // Importa tu componente de administración de tareas aquí
 
@@ -11,7 +11,7 @@ function App() {
       <div>
         <Navbar /> {/* Renderiza tu barra de navegación */}
         <Switch>
-          <Route exact path="/" component={Home} /> {/* Página de inicio */}
+         {/*<Route exact path="/" component={Home} /> {/* Página de inicio */}
           <Route path="/registro-tecnico" component={TechnicianRegistration} /> {/* Registro de técnicos */}
           <Route path="/administrar-tareas" component={AdminTasksManagement} /> {/* Administración de tareas */}
         </Switch>
